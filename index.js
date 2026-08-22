7b6064059d26d8c6e166a2b28cc2c8480d81bbf4
ImSmsek <smsek32000@gmail.com>
2026-08-22 06:34:04 +0300

    Create Minecraft bot with HTTP server
    This script creates a Minecraft bot that connects to a server and keeps it active by jumping periodically. It also sets up a simple HTTP server to indicate that the bot is running.

---
+++ index.js
@@ -0,0 +1,33 @@
+const mineflayer = require('mineflayer');
+const http = require('http');
+
+// Render kapanmasın diye web portu açıyoruz
+http.createServer((req, res) => {
+    res.write("Bot 7/24 Aktif!");
+    res.end();
+}).listen(process.env.PORT || 3000);
+
+function createBot() {
+    const bot = mineflayer.createBot({
+        host: 'SimsekOriginal.aternos.me',
+        port: 25565,
+        username: 'SimsekOriginalBot'
+    });
+
+    bot.on('spawn', () => {
+        console.log('Bot sunucuya girdi!');
+        setInterval(() => {
+            bot.setControlState('jump', true);
+            setTimeout(() => bot.setControlState('jump', false), 500);
+        }, 30000);
+    });
+
+    bot.on('end', () => {
+        console.log('Bağlantı koptu, tekrar deneniyor...');
+        setTimeout(createBot, 5000);
+    });
+
+    bot.on('error', err => console.log(err));
+}
+
+createBot();
