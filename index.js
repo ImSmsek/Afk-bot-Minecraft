a8c411c850763aa036aad8ba41e381a4e4e1b330
ImSmsek <smsek32000@gmail.com>
2026-08-23 08:07:13 +0300

    Refactor bot connection and remove HTTP server
    Removed HTTP server code and updated bot connection logic.

--- index.js
+++ index.js
@@ -1,35 +1,21 @@
 const mineflayer = require('mineflayer');
-const http = require('http');
-
-http.createServer((req, res) => {
-    res.write("Bot 7/24 Aktif!");
-    res.end();
-}).listen(process.env.PORT || 3000);
 
 function createBot() {
     console.log('Sunucuya bağlanılıyor...');
 
     const bot = mineflayer.createBot({
         host: 'SimsekOriginal.aternos.me',
-        port: 63781, // Aternos dinamik Java portu
         username: 'SimsekOriginalBot',
-        version: false, // Geyser/ViaVersion otomatik sürüm eşleme
-        checkTimeoutInterval: 60 * 1000
+        version: '1.21.1'
     });
 
     bot.on('spawn', () => {
         console.log('>>> BOT SUNUCUYA BAŞARIYLA GİRDİ! <<<');
-        setInterval(() => {
-            if (bot.entity) {
-                bot.setControlState('jump', true);
-                setTimeout(() => bot.setControlState('jump', false), 500);
-            }
-        }, 30000);
     });
 
     bot.on('end', (reason) => {
-        console.log('Bağlantı koptu, Sebep:', reason);
-        setTimeout(createBot, 10000);
+        console.log('Bağlantı koptu:', reason);
+        setTimeout(createBot, 5000);
     });
 
     bot.on('error', (err) => {
